b6ffb082b188b2183df0305e726ec1942417654a
Brice <git.dev@bricematter.com>
2026-01-10 17:58:48 +0100

    Initial Commit

---
+++ MonNavigateur/MonNavigateurApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonNavigateurApp.swift
+//  MonNavigateur
+//
+//  Created by BriceM4 on 10/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct MonNavigateurApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
