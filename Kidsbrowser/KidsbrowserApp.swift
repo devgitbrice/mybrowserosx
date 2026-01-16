87202a1d9866538bdaa0b7017823815677f18760
Brice <git.dev@bricematter.com>
2026-01-16 18:24:16 +0100

    Initial Commit

---
+++ Kidsbrowser/KidsbrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  KidsbrowserApp.swift
+//  Kidsbrowser
+//
+//  Created by BriceM4 on 16/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct KidsbrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
